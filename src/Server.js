0733b5c29afcaead99719cb3930de983eecd687a
hvlxh <monsta22019@gmail.com>
2023-03-03 20:47:03 +0530

    License Header and Console Title.

--- src/Server.js
+++ src/Server.js
@@ -1,3 +1,15 @@
+/**
+ *  _                 __ __  __  _____ ____  ______
+ * | |               / _|  \/  |/ ____|  _ \|  ____|
+ * | |     ___  __ _| |_| \  / | |    | |_) | |__
+ * | |    / _ \/ _` |  _| |\/| | |    |  _ <|  __|
+ * | |___|  __/ (_| | | | |  | | |____| |_) | |____
+ * |______\___|\__,_|_| |_|  |_|\_____|____/|______|
+ *
+ * Copyright 2023 hvlxh
+ * Github: https://github.com/LeafMCBE/Server
+ */
+
 import Protocol from "bedrock-protocol";
 import ResourcePackClientResponse from "./packets/handler/ResourcePackClientResponse.js";
 import fs, { existsSync } from "fs";
@@ -83,6 +95,15 @@ World:
 
       this.lang = new LangSelector(this.config);
     });
+
+    console.log(`
+ _                 __ __  __  _____ ____  ______ 
+| |               / _|  \\/  |/ ____|  _ \\|  ____|
+| |     ___  __ _| |_| \\  / | |    | |_) | |__   
+| |    / _ \\/ _\` |  _| |\\/| | |    |  _ <|  __|  
+| |___|  __/ (_| | | | |  | | |____| |_) | |____ 
+|______\\___|\\__,_|_| |_|  |_|\\_____|____/|______|
+`);
   }
 
   async start() {
